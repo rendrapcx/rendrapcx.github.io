fc573c606e1d4cd1e92a1e1af8d495230463bcfb
Rendra Pratama <rendrapc33@gmail.com>
2022-07-07 12:33:23 +0700

    update build

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-20220707.110609';
+const cacheName = 'chirpy-20220707.123255';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
